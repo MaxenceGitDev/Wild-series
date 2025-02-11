53447d8f5beb8c9169efef88e7cc8e0db313296e
MaxenceGitDev <maxence.pruvot@icloud.com>
2025-02-11 17:06:40 +0100

    step 6 done

--- server/src/modules/category/categoryActions.ts
+++ server/src/modules/category/categoryActions.ts
@@ -1,3 +1,6 @@
+//Import access to data
+import categoryRepository from "./categoryRepository";
+
 // Some data to make the trick
 
 const categories = [
@@ -11,12 +14,14 @@ const categories = [
   },
 ];
 
-// Declare the actions
+//Declare the actions
 
 import type { RequestHandler } from "express";
 
-const browse: RequestHandler = (req, res) => {
-  res.json(categories);
+const browse: RequestHandler = async (req, res) => {
+  const categoriesFromDB = await categoryRepository.readAll();
+
+  res.json(categoriesFromDB);
 };
 
 const read: RequestHandler = (req, res) => {
--- server/src/modules/program/programActions.ts
+++ server/src/modules/program/programActions.ts
@@ -1,3 +1,4 @@
+import programRepository from "./programRepository";
 // Some data to make the trick
 
 const programs = [
@@ -27,15 +28,21 @@ const programs = [
 
 import type { RequestHandler } from "express";
 
-const browse: RequestHandler = (req, res) => {
-  if (req.query.q != null) {
-    const filteredPrograms = programs.filter((program) =>
-      program.synopsis.includes(req.query.q as string),
-    );
-    res.json(filteredPrograms);
-  } else {
-    res.json(programs);
-  }
+// const browse: RequestHandler = (req, res) => {
+//   if (req.query.q != null) {
+//     const filteredPrograms = programs.filter((program) =>
+//       program.synopsis.includes(req.query.q as string),
+//     );
+//     res.json(filteredPrograms);
+//   } else {
+//     res.json(programs);
+//   }
+// };
+
+const browse: RequestHandler = async (req, res) => {
+  const programsFromDB = await programRepository.readAll();
+
+  res.json(programsFromDB);
 };
 
 const read: RequestHandler = (req, res) => {
