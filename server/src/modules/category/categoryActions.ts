adb17511063217e3af8011086ef931ba0b7116fe
MaxenceGitDev <maxence.pruvot@icloud.com>
2025-02-11 11:54:18 +0100

    step 4 done

---
+++ server/src/modules/category/categoryActions.ts
@@ -0,0 +1,35 @@
+// Some data to make the trick
+
+const categories = [
+	{
+		id: 1,
+		name: "Comédie",
+	},
+	{
+		id: 2,
+		name: "Science-Fiction",
+	},
+];
+
+// Declare the actions
+
+import type { RequestHandler } from "express";
+
+const browse: RequestHandler = (req, res) => {
+	res.json(categories);
+};
+
+const read: RequestHandler = (req, res) => {
+	const parseId = Number.parseInt(req.params.id);
+	const category = categories.find((c) => c.id === parseId);
+
+	if (category != null) {
+		res.json(category);
+	} else {
+		res.sendStatus(404);
+	}
+};
+
+// Export them to import them somewhere else
+
+export default { browse, read };
